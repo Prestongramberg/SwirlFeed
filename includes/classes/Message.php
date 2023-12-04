792478297b3fbc57bd630e97d76d6fb839a9a3d4
prestongramberg <knighttraintint@gmail.com>
2023-12-03 23:57:10 -0600

    Add: Display messages

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -38,15 +38,40 @@ class Message
         }
     }
 
-    public function sendMessage($user_to, $body, $date) {
-
-        if($body != "") {
+    public function sendMessage($user_to, $body, $date)
+    {
+        if ($body != "") {
             $userLoggedIn = $this->user_obj->getUsername();
-            $query = mysqli_query($this->con, "INSERT INTO messages VALUES(NULL, '$user_to', '$userLoggedIn', '$body', '$date', 'no', 'no', 'no')");
+            $query = mysqli_query(
+                $this->con,
+                "INSERT INTO messages VALUES(NULL, '$user_to', '$userLoggedIn', '$body', '$date', 'no', 'no', 'no')"
+            );
         }
     }
 
+    public function getMessages($otherUser)
+    {
+        $userLoggedIn = $this->user_obj->getUsername();
+        $data = "";
 
+        $query = mysqli_query(
+            $this->con,
+            "UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$otherUser'"
+        );
 
+        $get_message_query = mysqli_query(
+            $this->con,
+            "SELECT * FROM massages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser') OR (user_from='$userLoggedIn' AND user_to='$otherUser')"
+        );
+        while($row = mysqli_fetch_array($get_message_query)) {
+            $user_to = $row['user_to'];
+            $user_from = $row['user_from'];
+            $body = $row['body'];
+
+            $div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>" : "<div class='message' id='blue'>";
+            $data = $data . $div_top . $body . "</div><br><br>";
+        }
+        return $data;
+    }
 
 }
