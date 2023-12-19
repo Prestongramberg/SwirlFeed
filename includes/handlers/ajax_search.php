bd5c6595ba2af23b7ccad75b40dd1887589a4ed7
prestongramberg <knighttraintint@gmail.com>
2023-12-19 13:55:38 -0600

    Code Format

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -16,27 +16,30 @@ if (strpos($query, '_') !== false) {
         "SELECT * FROM users WHERE username LIKE '$query%' AND user_closed='no' LIMIT 8"
     );
 } // If there are two words, assume they are first and last names respectively
-else if (count($names) == 2) {
-    $usersReturnedQuery = mysqli_query(
-        $con,
-        "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed='no' LIMIT 8"
-    );
-} // If query has one word only. search first names or last names
 else {
-    $usersReturnedQuery = mysqli_query(
-        $con,
-        "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' OR last_name LIKE '%$names[0]%') AND user_closed='no' LIMIT 8"
-    );
+    if (count($names) == 2) {
+        $usersReturnedQuery = mysqli_query(
+            $con,
+            "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[1]%') AND user_closed='no' LIMIT 8"
+        );
+    } // If query has one word only. search first names or last names
+    else {
+        $usersReturnedQuery = mysqli_query(
+            $con,
+            "SELECT * FROM users WHERE (first_name LIKE '%$names[0]%' OR last_name LIKE '%$names[0]%') AND user_closed='no' LIMIT 8"
+        );
+    }
 }
 
-if($query != "") {
-    while($row = mysqli_fetch_array($usersReturnedQuery)) {
+if ($query != "") {
+    while ($row = mysqli_fetch_array($usersReturnedQuery)) {
         $user = new User($con, $userLoggedIn);
 
-        if($row['username'] != $userLoggedIn)
+        if ($row['username'] != $userLoggedIn) {
             $mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
-        else
+        } else {
             $mutual_friends = "";
+        }
 
         echo "<div class='resultDisplay'>
                     <a href='" . $row['username'] . "' style='color: #1485BD'>
